b30cfdb74cb3fc715bddd520cdbfb47444db3504
Ubaft <32735908+Ubaft@users.noreply.github.com>
2017-10-15 00:49:55 +0200

    Zkráceno jak to jen šlo. ještě se pokusim to zkrátit víc někdy :)

--- snake.cs
+++ snake.cs
@@ -11,24 +11,18 @@ namespace Snake_std
         static char food = 'X';
         static char player = 'O';
         static ConsoleKeyInfo Keyinfo;
-        static int[] xsize = new int[] { 3, 2};
-        static int[] ysize = new int[] { 3, 3};
+        static int[] xsize = new int[] { 3, 2 };
+        static int[] ysize = new int[] { 3, 3 };
         static int foodx;
         static int foody;
 
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Snake game: press Enter);
+            Console.WriteLine("Snake game: press Enter");
             Console.ReadLine();
             while (true)
             {
-                do
-                {
-                    Random rnd = new Random();
-                    foodx = rnd.Next(1, Console.WindowWidth - 2);
-                    foody = rnd.Next(1, Console.WindowHeight - 1);
-                } while (xsize.Contains(foodx) && ysize.Contains(foody));
+                Generator();
                 moveright();
             }
         }
@@ -36,38 +30,12 @@ namespace Snake_std
         {
             while (true)
             {
-                check(0);
-                CheckSnake(0);
-                
-                WriteFood();
-                for (int i = 0; i < xsize.Count(); i++)
-                {
-                    Console.SetCursorPosition(xsize[i], ysize[i]);
-                    Console.Write(player);
-                }
-                Grow();
-                Generator();
-                //vykreslení hada (DOPRAVA)
-
-                System.Threading.Thread.Sleep(100);
-                Console.Clear();
-
-                posun(0);
-                xsize[0] = xsize[1] + 1;
-                //posunutí arrayú (DOPRAVA)
+                dostuff(0);
+                xsize[0] = xsize[1] + 1;              
 
                 if (Console.KeyAvailable)
                 {
-                    Keyinfo = Console.ReadKey();
-                    switch (Keyinfo.Key)
-                    {
-                        case ConsoleKey.UpArrow:
-                            moveup();
-                            break;
-                        case ConsoleKey.DownArrow:
-                            movedown();
-                            break;
-                    }
+                    chooseUD();
                 }
             }
         }
@@ -75,79 +43,24 @@ namespace Snake_std
         {
             while (true)
             {
-                check(0);
-                CheckSnake(0);
-               
-                WriteFood();
-                for (int i = 0; i < xsize.Count(); i++)
-                {
-                    Console.SetCursorPosition(xsize[i], ysize[i]);
-                    Console.Write(player);
-                }
-                Grow();
-                Generator();
-                //vykreslení hada (DOLEVA)
-
-                System.Threading.Thread.Sleep(100);
-                Console.Clear();
-
-                posun(0);
-                xsize[0] = xsize[1] - 1;
-                //posunutí arrayú (DOLEVA)
+                dostuff(0);
+                xsize[0] = xsize[1] - 1;               
 
                 if (Console.KeyAvailable)
                 {
-                    Keyinfo = Console.ReadKey();
-                    switch (Keyinfo.Key)
-                    {
-                        case ConsoleKey.UpArrow:
-                            moveup();
-                            break;
-                        case ConsoleKey.DownArrow:
-                            movedown();
-                            break;
-                    }
+                    chooseUD();
                 }
-
-
             }
         }
         public static void moveup()
         {
             while (true)
             {
-                check(0);
-                CheckSnake(0);
-                
-                WriteFood();
-                for (int i = 0; i < xsize.Count(); i++)
-                {
-                    Console.SetCursorPosition(xsize[i], ysize[i]);
-                    Console.Write(player);
-                }
-                Grow();
-                Generator();
-                //vykreslení hada (NAHORU)
-
-                System.Threading.Thread.Sleep(100);
-                Console.Clear();
-
-                posun(0);
-                ysize[0] = ysize[1] - 1;
-                //posunutí arrayú (NAHORU)
-
+                dostuff(0);
+                ysize[0] = ysize[1] - 1;              
                 if (Console.KeyAvailable)
                 {
-                    Keyinfo = Console.ReadKey();
-                    switch (Keyinfo.Key)
-                    {
-                        case ConsoleKey.LeftArrow:
-                            moveleft();
-                            break;
-                        case ConsoleKey.RightArrow:
-                            moveright();
-                            break;
-                    }
+                    chooseRL();
                 }
             }
         }
@@ -155,79 +68,62 @@ namespace Snake_std
         {
             while (true)
             {
-                check(0);
-                CheckSnake(0);
-                
-                WriteFood();
-                for (int i = 0; i < xsize.Count(); i++)
-                {
-                    Console.SetCursorPosition(xsize[i], ysize[i]);
-                    Console.Write(player);
-                }
-                Grow();
-                Generator();
-                //vykreslení hada (DOLU)
-
-                System.Threading.Thread.Sleep(100);
-                Console.Clear();
-
-                posun(0);
-                ysize[0] = ysize[1] + 1;
-                //posunutí arrayú (DOLU)
-
-
+                dostuff(0);
+                ysize[0] = ysize[1] + 1;              
                 if (Console.KeyAvailable)
                 {
-                    Keyinfo = Console.ReadKey();
-                    switch (Keyinfo.Key)
-                    {
-                        case ConsoleKey.LeftArrow:
-                            moveleft();
-                            break;
-                        case ConsoleKey.RightArrow:
-                            moveright();
-                            break;
-                    }
+                    chooseRL();
                 }
             }
-
         }
-        public static int posun(int a)
+        public static void chooseRL()
         {
-            for (int i = xsize.Count() - 1; i > 0; i--)
+            Keyinfo = Console.ReadKey();
+            switch (Keyinfo.Key)
             {
-                xsize[i] = xsize[i - 1];
-                ysize[i] = ysize[i - 1];
+                case ConsoleKey.LeftArrow:
+                    moveleft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    moveright();
+                    break;
             }
-            return 0;
+        }
+        public static void chooseUD()
+        {
+            Keyinfo = Console.ReadKey();
+            switch (Keyinfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    moveup();
+                    break;
+                case ConsoleKey.DownArrow:
+                    movedown();
+                    break;
+            }
         }
         public static void Generator()
-        {
-            if (xsize[0] == foodx && ysize[0] == foody)
-            {
+        {          
                 do
                 {
                     Random rnd = new Random();
                     foodx = rnd.Next(1, Console.WindowWidth - 2);
                     foody = rnd.Next(1, Console.WindowHeight - 1);
-                } while (xsize.Contains(foodx) && ysize.Contains(foody));
-            }
-        }
-        public static void WriteFood()
+                } while (xsize.Contains(foodx) && ysize.Contains(foody));          
+        }     
+        public static void EndGame()
         {
-            Console.SetCursorPosition(foodx, foody);
-            Console.Write(food);
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("GAME ENDED");
+            Environment.Exit(0);
         }
-        public static int check(int a)
+        public static int dostuff (int a)
         {
             if (xsize[0] == -1 || ysize[0] == -1 || xsize[0] == Console.WindowWidth || ysize[0] == Console.WindowHeight)
             {
                 EndGame();
             }
-            return 0;
-        }
-        public static int CheckSnake(int a)
-        {
             for (int i = 1; i < xsize.Length; ++i)
             {
                 if (xsize[0] == xsize[i] && ysize[0] == ysize[i])
@@ -235,10 +131,13 @@ namespace Snake_std
                     EndGame();
                 }
             }
-            return 0;
-        }
-        public static void Grow()
-        {
+            Console.SetCursorPosition(foodx, foody);
+            Console.Write(food);
+            for (int i = 0; i < xsize.Count(); i++)
+            {
+                Console.SetCursorPosition(xsize[i], ysize[i]);
+                Console.Write(player);
+            }
             if (xsize[0] == foodx && ysize[0] == foody)
             {
                 Array.Resize(ref xsize, xsize.Length + 1);
@@ -246,13 +145,18 @@ namespace Snake_std
                 xsize[xsize.Length - 1] = xsize[xsize.Length - 2] + 1;
                 ysize[ysize.Length - 1] = ysize[ysize.Length - 2] + 1;
             }
-        }
-        public static void EndGame()
-        {
+            if (xsize[0] == foodx && ysize[0] == foody)
+            {
+                Generator();
+            }
+            System.Threading.Thread.Sleep(100);
             Console.Clear();
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine("GAME ENDED");
-            Environment.Exit(0);
+            for (int i = xsize.Count() - 1; i > 0; i--)
+            {
+                xsize[i] = xsize[i - 1];
+                ysize[i] = ysize[i - 1];
+            }
+            return 0;
         }
     }
 }
